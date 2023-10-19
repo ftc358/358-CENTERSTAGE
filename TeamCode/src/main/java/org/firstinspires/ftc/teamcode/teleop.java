b3d484b2239c85f9db1162049057409f7b182345
Kevin Wang <51310353+wangjkevin@users.noreply.github.com>
2023-10-19 16:05:08 -0700

    test intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
@@ -116,7 +116,7 @@ public class teleop extends LinearOpMode{
             telemetry.update();
         }
     }
-}
+
 
 
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testintake.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+@TeleOp
+public class testintake extends LinearOpMode {
+    DcMotor intake;
+    public void runOpMode() {
+        waitForStart();
+        intake = hardwareMap.get(DcMotor.class, "intake");
+
+        while (opModeIsActive()) {
+            ////////////////// INTAKE //////////////////
+
+            // IN //
+            if (gamepad2.left_stick_y < -0.5) {
+                hardware358.intake.setPower(0.3);
+                telemetry.addData(">", "intaking...");
+
+            }
+            // OUT //
+            else if (gamepad2.left_stick_y > 0.5){
+                hardware358.intake.setPower(-0.3);
+                telemetry.addData(">", "spitting out pixels...");
+            }
+            // at rest //
+            else {
+                hardware358.intake.setPower(0);
+                telemetry.addData(">", "not intaking");
+
+            }
+        }
+        // Update telemetry to show the current state
+        telemetry.update();
+
+        }
+
+
+    }
+
+}
